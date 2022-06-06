5340acfa2aa2d32360bc466bacda6ce55d75d122
kaanberke34 <berkeyilmaz34@gmail.com>
2022-06-06 20:53:28 +0300

    website

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Work in Progress" />
+</template>
+
+<style>
+:root {
+  background-color: #2c3e50;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: snow;
+  margin-top: 60px;
+}
+</style>
